d6444c587d3a8478de80de13b836bc2c3f8b60cb
Frank Peter Schultze <frank.peter.schultze@gmail.com>
2016-04-30 21:12:29 +0200

    First commit

---
+++ Test-DBNull.ps1
@@ -0,0 +1,38 @@
+﻿<#
+.SYNOPSIS
+    Test whether a value equals DBNull.
+
+.DESCRIPTION
+    With Test-DBNull you can test whether a database field has a valid value.
+
+    If you deal with databases you may need to determine whether a database
+    field has a nonexistent value. A database field might not contain any data.
+    That is, the field is considered to exist but not having a value. The
+    DBNull class represents a nonexistent value. Test-DBNull tests the given
+    value against a DBNull reference object.
+
+.EXAMPLE
+    Test-DBNull -Value $queryResult.Status
+
+.EXAMPLE
+    $queryResult.Status | Test-DBNull
+
+.INPUTS
+    System.Object
+
+.OUTPUTS
+    System.Boolean
+#>
+function Test-DBNull
+{
+    [CmdletBinding()]
+    [OutputType([System.Boolean])]
+    Param
+    (
+        # Identifies the value to inspect
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [System.Object]
+        $Value
+    )
+    ([System.DBNull]::Value).Equals($Value)
+}
---
+++ Test/Test-DBNull.Tests.ps1
@@ -0,0 +1,20 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace('.Tests.', '.')
+. "$here\..\$sut"
+
+Describe 'Test-DBNull' {
+
+    It 'returns False' {
+
+        $testValue = ''
+
+        Test-DBNull -Value $testValue | Should Be $false
+    }
+
+    It 'returns True' {
+
+        $testValue = [System.DBNull]::Value
+
+        Test-DBNull -Value $testValue | Should Be $true
+    }
+}
